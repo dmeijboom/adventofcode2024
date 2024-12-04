53ff286693fd7caf1207b78b78f205417dd7d132
Dillen Meijboom <dillen@brainhive.nl>
2024-12-04 21:29:45 +0100

    feat: add day-three part two

--- day-three/src/main.rs
+++ day-three/src/main.rs
@@ -1,9 +1,28 @@
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
+    let mut enabled = true;
     let sum = INPUT
         .split("mul(")
         .into_iter()
+        .filter_map(|line| {
+            let skip = !enabled;
+
+            match (line.find("do()"), line.find("don't()")) {
+                (Some(do_idx), Some(dont_idx)) => {
+                    enabled = do_idx > dont_idx;
+                }
+                (Some(_), None) => enabled = true,
+                (None, Some(_)) => enabled = false,
+                _ => (),
+            }
+
+            if skip {
+                return None;
+            }
+
+            Some(line)
+        })
         .filter(|input| matches!(input.chars().next(), Some('0'..='9')))
         .filter_map(|input| input.split_once(')'))
         .filter_map(|(input, _)| input.split_once(','))
